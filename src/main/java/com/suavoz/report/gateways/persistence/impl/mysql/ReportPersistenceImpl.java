544bb77bd891de3a23d50ab13b4c8cd993a63dbe
Gustavo <gusfeh07@gmail.com>
2022-03-20 00:02:17 -0300

    feat: criação do repository e impl com mysql

---
+++ src/main/java/com/suavoz/report/gateways/persistence/impl/mysql/ReportPersistenceImpl.java
@@ -0,0 +1,24 @@
+package com.suavoz.report.gateways.persistence.impl.mysql;
+
+import com.suavoz.report.domain.Report;
+import com.suavoz.report.gateways.persistence.ReportPersistenceGateway;
+
+import java.util.List;
+
+public class ReportPersistenceImpl implements ReportPersistenceGateway {
+
+    @Override
+    public Report save(Report report) {
+        return null;
+    }
+
+    @Override
+    public void delete(Report report) {
+
+    }
+
+    @Override
+    public List<Report> findAll() {
+        return null;
+    }
+}
